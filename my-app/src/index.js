998b2e03cdda5de7942c325e83dd017e94d33d66
Moninar <33311166+Moninar@users.noreply.github.com>
2019-04-19 10:28:47 -0700

    set up Parent/Child components

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import MyInfo from "./components/MyInfo";
+import App from "./components/App";
 
-ReactDOM.render(<MyInfo />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
