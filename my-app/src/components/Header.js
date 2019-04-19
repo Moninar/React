f4145fa0c50c4e5b9986e6463131207558d0a917
Moninar <33311166+Moninar@users.noreply.github.com>
2019-04-19 12:24:37 -0700

    set up className

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react"
 function Header() {
     return (
         <nav>
-            <h1>This is the header</h1>
+            <h1 className="navheader">This is the header</h1>
             <ul>
                 <li>1</li>
                 <li>2</li>
