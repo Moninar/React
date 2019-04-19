c3ab561dfd9ac1bc5a39a0760d8ee57e5994fd49
Moninar <33311166+Moninar@users.noreply.github.com>
2019-04-19 13:01:45 -0700

    automatically change the style and content with JavaScript at the same time

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,15 +1,27 @@
 import React from "react"
 
 function Footer() {
-    const style = {
-        color: "#FF8C00",
-        backgroundColor: "red",
-        fontSize: 50
+    const date = new Date(1999, 10, 1, 17);
+    const hours = date.getHours();
+    let timeOfDay;
+    const styles = {
+        backgroundColor: "grey"
+    }
+
+    if (hours < 12) {
+        timeOfDay = "morning";
+        styles.backgroundColor = "red";
+    } else if (hours >= 12 && hours < 17) {
+        timeOfDay = "afternoon";
+        styles.backgroundColor = "green";
+    } else {
+        timeOfDay = "night";
+        styles.backgroundColor = "grey";
     }
 
     return (
         <footer>
-            <h3 style={style}>This is my footer element</h3>
+            <h3 style={styles}>Good {timeOfDay}</h3>
         </footer>
     )
 }
