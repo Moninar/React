b8675ac825d7663394466ccb63117fd72769e7b2
Moninar <33311166+Moninar@users.noreply.github.com>
2019-04-24 23:28:51 -0700

    conditionally style using &&

--- my-app/src/components/Joke.js
+++ my-app/src/components/Joke.js
@@ -3,8 +3,8 @@ import React from "react"
 function Joke(props) {
     return (
         <div>
-            <h3 style={{display: props.question ? "block" : "none"}}>question: {props.question}</h3>
-            <p>answer: {props.punchLine}</p>
+            <h3 style={{display: !props.question && "none"}}>question: {props.question}</h3>
+            <p style={{color: !props.question && "#888888"}}>answer: {props.punchLine}</p>
         </div>
     )
 }
