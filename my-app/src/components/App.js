bb36b5ded00c26764f685557cd43b95b2e84a0f8
Moninar <33311166+Moninar@users.noreply.github.com>
2019-04-25 15:57:22 -0700

    add click event handler on JSX

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -7,28 +7,44 @@ import jokesData from "./jokesData"
 import productsData from "./productsData"
 import Product from "./Product"
 
-function App() {
-    const jokeComponents = jokesData.map(function(joke) {
-        return (
-            <Joke key = {joke.id} question={joke.question} punchLine={joke.punchLine} />
-        )
-    })
+class App extends React.Component {
+    constructor() {
+        super() 
+        this.state = {
 
-    const productsComponents = productsData.map((product) => {
-        return (
-            <Product key={product.id} name={product.name} price={product.price} description={product.description} />
-        )
-    })
+        }
+    }
 
-    return (
-        <div>
-            <Header />
-            <MainContent />
-            {jokeComponents}
-            {productsComponents}
-            <Footer />
-        </div>
-    )
+    render() {
+        const jokeComponents = jokesData.map(function(joke) {
+            return (
+                <Joke key = {joke.id} question={joke.question} punchLine={joke.punchLine} />
+            )
+        })
+    
+        const productsComponents = productsData.map((product) => {
+            return (
+                <Product key={product.id} name={product.name} price={product.price} description={product.description} />
+            )
+        })
+    
+        return (
+            <div>
+                <Header />
+                <button onClick={handleClick}>Click me</button>
+                <MainContent />
+                {jokeComponents}
+                {productsComponents}
+                <Footer />
+            </div>
+        )
+    }
+
+
+}
+
+function handleClick() {
+    console.log("I was clicked")
 }
 
 export default App
