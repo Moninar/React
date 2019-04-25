b6d4e2d63d34510e659a6731720b5755f4d11888
Moninar <33311166+Moninar@users.noreply.github.com>
2019-04-25 14:52:03 -0700

    state concept*

--- my-app/src/components/Product.js
+++ my-app/src/components/Product.js
@@ -1,12 +1,21 @@
 import React from "react"
 
 class Product extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            answer: "Yes"
+        }
+    }
+
     render() {
         return (
             <div>
                 <p>name: {this.props.name}</p>
                 <p>price: {this.props.price}</p>
                 <p>description: {this.props.description}</p>
+                <p>answer: {this.state.answer}</p>
+                
             </div>
         )
     }
