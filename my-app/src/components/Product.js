f67866f3abddca5f9edd12b4cc50c24c30c841b9
Moninar <33311166+Moninar@users.noreply.github.com>
2019-04-25 14:23:42 -0700

    get a sip of class-based component

--- my-app/src/components/Product.js
+++ my-app/src/components/Product.js
@@ -1,13 +1,15 @@
 import React from "react"
 
-function Product(props) {
-    return (
-        <div>
-            <p>name: {props.name}</p>
-            <p>price: {props.price}</p>
-            <p>description: {props.description}</p>
-        </div>
-    )
+class Product extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>name: {this.props.name}</p>
+                <p>price: {this.props.price}</p>
+                <p>description: {this.props.description}</p>
+            </div>
+        )
+    }
 }
 
 export default Product
